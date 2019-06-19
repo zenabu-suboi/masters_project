156365986e654fc30193ea42c10a23f867b3de9a
zenabu suboi <zenabu@sun.ac.za>
2019-06-19 16:34:21 +0200

    obtain new targets

--- R_CODES/PHASE 2/obtaining_targets.R
+++ R_CODES/PHASE 2/obtaining_targets.R
@@ -2,30 +2,16 @@
 library(EasyABC)
 library(SimInf)
 
-### create function to use in ABC_mcmc
-modelABC= function(parameters, N, inf=0.1, I=inf*N, samplesize){
-  library(SimInf)
-  u0 = data.frame(S=c(90000), I=c(10000), R=c(0))    #specifying initial compartmental values
-  model <- SIR(u0, 1:75, beta=parameters[1], gamma=parameters[2]) # specifying the model
-  result <- run(model, threads = 1)     # running the model
-  pop= numeric()
-  pop[1]=result@U[2,50]   # number of infected individuals at time 50
-  pop[2]=result@U[2,75]  # number of infected individuals at time 75
-  prevs=pop/sum(u0) # prevalence at times 50 and 75
-  return(prevs)
-}
-
-pop= numeric()
-### try running it once, should return two population prevalence percentages
-modelforABCmcmc2(c(0.2,0.02))
-
 ##############################################################
+################################################################
+
 library(deSolve)
 
-modelABC <- function(t, x, parms)  {
+modelABC <- function(parameters) {
+sir <- function(t, x, parms)  {
   with(as.list(c(parms, x)), {
     N = S+I+R
-    dS = -beta * I/N * S
+    dS = -1 * beta * I/N * S
     dI = beta * I/N * S - gamma * I
     dR = gamma * I
     dCumI = beta * I/N * S
@@ -33,80 +19,62 @@ modelABC <- function(t, x, parms)  {
     list(output)
   })
 }
+start<-c(S=990, I=10, R=0, CumI=0) #the Initial values
 
-#the Initial values
-start<-c(S=990, I=10, R=0, CumI=0)
+times <- seq(0, 75, 1) # time period 
+parms <- c(beta= parameters[1], gamma= parameters[2]) ## The parameters 
 
-## The parameters 
-times <- seq(0, 75, 1)
+run<-ode(times=times, y=start, func=sir,parms=parms) # ode solver
+Inc = c(0,diff(run[,5]))
+run1 = cbind(run,Inc) # adds incidence column to the results from te ode solver
 
-save_targets <- matrix(c(0,0,0),1000,3)
-for (i in 1:1000) {
-  parms = numeric()
-  parms[1] = rlnorm(1,0,1)
-  parms[2] = rlnorm(1,0,1)
-  run <- ode(times=times, y=start, func=modelABC,parms=parms)
-  save_targets[i,] = c(run[20,5],run[50,5], run[75,5])
+targ = numeric() # create empty vector called targ
+targ[1] = run1[21,6] # incidence at time 20
+targ[2] = run1[51,6]  # incidence at time 50
+targ[3] = run1[76,6] # incidence at time 75
+targ[4] = run1[76,5] # cummulatve incidence
+#targ[5] = #  peak incidence
+
+return(targ)
 }
 
+modelABC(c( runif(1,0,1) ,runif(1,0,0.5))) # run model to obtain targets once
 
-plot(run[,2], col="red", ylim=c(0,1000), type="l", main="SEIR Model")
-# you can also use matplot toplot evert column
-lines(run[,3], col="blue")
-lines(run[,4], col="green")
-lines(run[,5], col="purple")
-legend("topright",legend=c("S","E" ,"I", "R"), col=c("red","black", "blue", "green"), lty=c(1,1,1))
-Sys.sleep(0.3)
+#############################################################################
+# run model several times and save targets in a matrix
 
-
-
-
-
-
-
-
-parms <- c(beta=0.8, gamma=0.1)
-
-## vector of timesteps
-
-
-run<-ode(times=times, y=start, func=sir,parms=parms)
-
-
-
-
-##################################################################
-transitions <- c("S -> b*S*I/(S+I+R) -> I", "I -> g*I -> R")
- compartments <- c("S", "I", "R")
-
-
- m <- mparse(c("S -> b*S*I/(S+I+R) -> I + Icum", "I -> g*I -> R"),
-             + c("S", "I", "Icum", "R"), b = 0.16, g = 0.077)
- model <- init(m, cbind(S = 99, I = 1, Icum = 0, R = 0), 1:180)
- result <- run(model, threads = 1, seed = 22)
- plot(stepfun(result@tspan[-1], diff(c(0, U(result)["Icum",]))),
-         + main = "", xlab = "Time", ylab = "Number of cases",
-         + do.points = FALSE)
-
-
-
-
-
-
-################################################################
-
-### set.seed for reproducability
 set.seed(123)
 
 ### save the results from 10000 runs, take the means as the targets
-saveres= matrix(c(0,0),100,2)
+save_targets= matrix(c(0,0,0,0),100,4)
 for(i in 1:100){
-  saveres[i,]= modelforABCmcmc2(c(0.2,0.02))
+  save_targets[i,]= modelABC(c( runif(1,0,1) ,runif(1,0,0.5)))
 }
 
-### we call the target: truepop.prev 
-truepop.prev= c(ceiling(mean(saveres[,1])), ceiling(mean(saveres[,2])))
-truepop.prev
-plot(saveres[,1], saveres[,2], pch=16)
-points(mean(saveres[,1]), mean(saveres[,2]), cex=1.5, col="red", pch=16)
 
+####################################################################
+# mean targets
+ 
+targets = c(mean(save_targets[,1]), mean(save_targets[,2]), mean(save_targets[,3]),
+            mean(save_targets[,4]))
+
+######################################################################
+
+
+
+## rough work
+
+tail(run1)
+head(run1)
+sum(Inc)== subset(run[,5],times==75)
+
+head(save_targets)
+dim(save_targets)
+
+hist(save_targets[,1])
+hist(save_targets[,2])
+hist(save_targets[,3])
+hist(save_targets[,4])
+
+
+##################################################################
