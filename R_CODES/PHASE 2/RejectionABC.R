97369eb841b0a62c536bd7332e7530aa91b40bfc
zenabu suboi <zenabu@sun.ac.za>
2019-06-20 17:36:42 +0200

    ABC runs

--- R_CODES/PHASE 2/RejectionABC.R
+++ R_CODES/PHASE 2/RejectionABC.R
@@ -1,16 +1,65 @@
 
 library(EasyABC)
 
-nreprej= 1000
-tolp= 1  
+par(mfrow=c(3,2))
+
+set.seed(234)
+ABC_rej<-ABC_rejection(model=modelABC, prior=list(c("unif",0,1),c("unif",0,0.5)), 
+                       summary_stat_target=targets, nb_simul=100,
+                       tol=0.5, progress_bar = T)
+ABC_rej$computime
+plot(ABC_rej$param[, 1],ABC_rej$param[, 2], main = "nb_simul=100")
+
+
+#R0<-ABC_rej$param[, 1]/ ABC_rej$param[, 2]
+#plot(ABC_rej$param[, 2],R0, main = "nb_simul=100")
+
+
 
 
 set.seed(234)
 ABC_rej<-ABC_rejection(model=modelABC, prior=list(c("unif",0,1),c("unif",0,0.5)), 
-                       summary_stat_target=targets, nb_simul=nreprej,
-                       tol=tolp, progress_bar = T)
-
+                       summary_stat_target=targets, nb_simul=1000,
+                       tol=0.5, progress_bar = T)
 ABC_rej$computime
+plot(ABC_rej$param[, 1],ABC_rej$param[, 2], main = "nb_simul=1000")
 
-### plot the posterior obtained by ABC rejection
-plot(ABC_rej$param[, 1], ABC_rej$param[, 2])
+
+#R0<-ABC_rej$param[, 1]/ ABC_rej$param[, 2]
+#plot(ABC_rej$param[, 1], R0, main = "nb_simul=1000")
+
+
+
+set.seed(234)
+ABC_rej<-ABC_rejection(model=modelABC, prior=list(c("unif",0,1),c("unif",0,0.5)), 
+                       summary_stat_target=targets, nb_simul=10000,
+                       tol=0.5, progress_bar = T)
+ABC_rej$computime
+plot(ABC_rej$param[, 1],ABC_rej$param[, 2], main = "nb_simul=10000")
+
+
+#R0<-ABC_rej$param[, 1]/ ABC_rej$param[, 2]
+#plot(ABC_rej$param[, 1], R0, main = "nb_simul=10000")
+
+
+
+
+set.seed(234)
+ABC_rej<-ABC_rejection(model=modelABC, prior=list(c("unif",0,1),c("unif",0,0.5)), 
+                       summary_stat_target=targets, nb_simul=100000,
+                       tol=0.5, progress_bar = T)
+ABC_rej$computime
+plot(ABC_rej$param[, 1],ABC_rej$param[, 2], main = "nb_simul=100000")
+
+
+#R0<-ABC_rej$param[, 1]/ ABC_rej$param[, 2]
+#plot(ABC_rej$param[, 1], R0, main = "nb_simul=100000")
+
+
+
+set.seed(234)
+ABC_rej<-ABC_rejection(model=modelforABCmcmc2, prior=list(c("unif",0,1),c("unif",0,0.5)), 
+                       summary_stat_target=truepop.prev, nb_simul=1000000,
+                       tol=0.5, progress_bar = T)
+ABC_rej$computime
+plot(ABC_rej$param[, 1], ABC_rej$param[, 2], main = "nb_simul=1000000")
--- R_CODES/PHASE 2/SequentialLenABC.R
+++ R_CODES/PHASE 2/SequentialLenABC.R
@@ -1,14 +1,69 @@
 
 library(EasyABC)
 
-nrepseq=400
-#alpha=0.5 by default
+par(mfrow=c(2,2))
 set.seed(123)
-ABC_seq1<-ABC_sequential(method="Lenormand", model=modelABC,
-                         prior=list(c("unif",0,1), c("unif",0,0.5)),
-                         nb_simul=nrepseq, summary_stat_target= targets,
-                         p_acc_min=0.2, progress_bar = T)
+ABC_seq1<-ABC_sequential(method="Lenormand", model=modelABC, prior=list(c("unif",0,1),
+                         c("unif",0,0.5)), nb_simul=100,
+                         summary_stat_target=targets, p_acc_min=0.4, progress_bar = T)
 
-plot(ABC_seq1$param[, 1], ABC_seq1$param[, 2])
 
 ABC_seq1$computime
+plot(ABC_seq1$param[, 1], ABC_seq1$param[, 2], main ="#simul = 100")
+
+
+#R0<-ABC_seq1$param[, 1]/ ABC_seq1$param[, 2]
+#plot(R0,ABC_seq1$param[, 2], main = "nb_simul=100")
+
+
+
+
+set.seed(123)
+ABC_seq1<-ABC_sequential(method="Lenormand", model=modelABC, prior=list(c("unif",0,1),
+                                                                                c("unif",0,0.5)), nb_simul=1000,
+                         summary_stat_target=targets, p_acc_min=0.4, progress_bar = T)
+
+plot(ABC_seq1$param[, 1], ABC_seq1$param[, 2], main ="#simul = 1000")
+ABC_seq1$computime
+
+
+#R0<-ABC_seq1$param[, 1]/ ABC_seq1$param[, 2]
+#plot(R0,ABC_seq1$param[, 2], main = "nb_simul=1000")
+
+
+
+
+set.seed(123)
+ABC_seq1<-ABC_sequential(method="Lenormand", model=modelforABCmcmc2, prior=list(c("unif",0,1),
+                                                                                c("unif",0,0.5)), nb_simul=10000,
+                         summary_stat_target=truepop.prev, p_acc_min=0.4, progress_bar = T)
+
+R0<-ABC_seq1$param[, 1]/ ABC_seq1$param[, 2]
+plot(R0,ABC_seq1$param[, 2], main = "nb_simul=10000")
+
+plot(ABC_seq1$param[, 1], ABC_seq1$param[, 2], main ="#simul = 10000")
+ABC_seq1$computime
+
+
+set.seed(123)
+ABC_seq1<-ABC_sequential(method="Lenormand", model=modelforABCmcmc2, prior=list(c("unif",0,1),
+                                                                                c("unif",0,0.5)), nb_simul=100000,
+                         summary_stat_target=truepop.prev, p_acc_min=0.4, progress_bar = T)
+
+R0<-ABC_seq1$param[, 1]/ ABC_seq1$param[, 2]
+plot(R0,ABC_seq1$param[, 2], main = "nb_simul=10000")
+
+plot(ABC_seq1$param[, 1], ABC_seq1$param[, 2], main ="#simul = 100000")
+ABC_seq1$computime
+
+
+
+set.seed(123)
+ABC_seq1<-ABC_sequential(method="Lenormand", model=modelforABCmcmc2, prior=list(c("unif",0,1), 
+                                                                                c("unif",0,0.5)), nb_simul=1000000,
+                         summary_stat_target=truepop.prev, p_acc_min=0.4, progress_bar = T)
+
+plot(ABC_seq1$param[, 1], ABC_seq1$param[, 2], main ="#simul = 1000000")
+ABC_seq1$computime
+
+
