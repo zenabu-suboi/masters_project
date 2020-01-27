b23be3e1e709010479ebe69cce730f77a4273382
Suboi <zenabu@sun.ac.za>
2020-01-27 16:59:55 +0200

    changed the function in the ABCtargets package

--- ABCtargets/R/ABCtargets.R
+++ ABCtargets/R/ABCtargets.R
@@ -5,28 +5,36 @@
 ### create function to use in ABC_mcmc
 # scenario 1 == two targets
 
-targets2 <- function(parm1, parm2){
+targets <- function(parameters){
 
   library(SimInf)
 
-modelforABC <- function(parm1, parm2){
+  modelforABC = function(parameters,
+                         times=1:75,
+                         targetTimes=c(50,75),
+                         peakPrevalence = FALSE){
 
-  u0 = data.frame(S=c(990), I=c(10), R=c(0))
 
-  model <- SIR(u0, 1:75, beta = parm1,
-               gamma = parm2)
+    u0 = data.frame(S = c(990), # initial compartmental values
+                    I = c(10),
+                    R = c(0))
 
-  result <- run(model, threads = 1)#, seed=sample.int(1000000000,1))
-  prev   <- prevalence(result, I~.)
-  targ   <- numeric()
-  targ[1] <- prev[50,2]
-  targ[2] <- prev[75,2]
-  # peak_prev <- max(prev[,2])
+    model <- SIR(u0, # initial compartmental values
+                 times,                  # stochastic sir model that outputs different epicurves
+                 beta = parameters[1],      # per run
+                 gamma = parameters[2])
 
-  # pop[1]=result@U[2,30]
-  # pop[2]=result@U[2,60]
-  return(c(targ[1],targ[2]))
-}
+    result <- run(model,
+                  threads = 1)   # runs the SIR model and outputs results
+
+    prev <- prevalence(result, I~.)
+    # targ <- numeric()
+    targs <- prev[targetTimes,2]
+
+    if (peakPrevalence){return(c(targs,max(prev[,2])))}
+    else(return(targs))
+  }
+
 
 ### try running it once, should return two population prevalence percentages
 ##modelforABC(c(0.2,0.02))
@@ -37,66 +45,16 @@ modelforABC <- function(parm1, parm2){
 set.seed(123)
 
 ### save the results from 10000 runs, take the means as the targets
-saveres = matrix(c(0,0),100,2)
+savetargs = matrix(c(0,0),100,2)
 for(i in 1:100){
-  saveres[i,] = modelforABC(parm1,
+  savetargs[i,] = modelforABC(parm1,
                              parm2)
 }
 ### we call the target
-  targs2 = c(mean(saveres[,1]),
-             mean(saveres[,2]))
+  targs = c(mean(savetargs[,1]),
+             mean(savetargs[,2]))
 
-return(targs2)
-
-}
-
-
-#######################################################################
-
-
-targets3 <- function(parm1, parm2){
-
-  library(SimInf)
-
-  modelforABC <- function(parm1, parm2){
-
-    u0 = data.frame(S=c(990), I=c(10), R=c(0))
-
-    model <- SIR(u0, 1:75, beta = parm1,
-                 gamma = parm2)
-
-    result <- run(model, threads = 1)#, seed=sample.int(1000000000,1))
-    prev   <- prevalence(result, I~.)
-    targ   <- numeric()
-    targ[1] <- prev[50,2]
-    targ[2] <- prev[75,2]
-    peak_prev <- max(prev[,2])
-
-    # pop[1]=result@U[2,30]
-    # pop[2]=result@U[2,60]
-    return(c(targ[1], targ[2], peak_prev))
-  }
-
-  ### try running it once, should return two population prevalence percentages
-  ##modelforABC(c(0.2,0.02))
-
-
-
-  ### set.seed for reproducability
-  set.seed(123)
-
-  ### save the results from 10000 runs, take the means as the targets
-  saveres = matrix(c(0,0,0),100,3)
-  for(i in 1:100){
-    saveres[i,] = modelforABC(parm1,
-                              parm2)
-  }
-  ### we call the target
-  targs3 = c(mean(saveres[,1]),
-             mean(saveres[,2]),
-             mean(saveres[,3]))
-
-  return(targs3)
+return(targets)
 
 }
-targets3(0.2, 0.02)
+
