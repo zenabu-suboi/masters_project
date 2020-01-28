3920cd23fa66f91c9349e2fe03953fb7613c3e2f
Suboi <zenabu@sun.ac.za>
2020-01-28 09:09:34 +0200

    added BMLE function

--- R_CODES/masters_project/my_functions.R
+++ R_CODES/masters_project/my_functions.R
@@ -37,3 +37,59 @@ modelforABC = function(parameters,
 
 
 
+
+###########################################################################################
+# 2. BMLE function
+
+#samSize <- 100
+
+library(SimInf)
+
+sir_bmle <- function(beta,
+                     gamma, 
+                     N = 1000,
+                     inf = 0.1, 
+                     sampleSize = 100){
+  
+  
+  u0 <- data.frame(S=N*(1-inf),
+                   I=N*inf, R=0)
+  
+  model <- SIR(u0, tspan = seq(0,75,by=1),
+               beta = beta, gamma=gamma)
+  
+  result <- run(model)
+  
+  individuals50 <- c(rep("S", result@U[1, 50]),
+                     rep("I", result@U[2, 50]),
+                     rep("R", result@U[3, 50]))
+  
+  individuals75 <- c(rep("S", result@U[1, 75]),
+                     rep("I", result@U[2, 75]),
+                     rep("R", result@U[3, 75]))
+  
+  
+  samplePop <- c(summary(as.factor
+                        (sample(individuals50,
+                        size = sampleSize))),
+                        summary(as.factor(sample(individuals75,
+                        size = sampleSize))))
+  
+  pop <- samplePop[names(samplePop) == "I"] #lret it return zero instead of numeric(0)
+  
+  # prev <- prevalence(result, I~.)
+  # peak_prev <- max(prev[,2])
+  
+  for(i in 1:2){
+    if(is.na(pop[i])){
+      pop[i] <- 0
+    }
+  }
+  
+  
+  return(pop)
+  
+}
+
+#sir_bmle(0.2, 0.02)
+
